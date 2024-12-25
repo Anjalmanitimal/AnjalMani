b68fe408abe8face2bb88b5abfaeb7de42c0a6f0
anjal <anjalmani2023@gmail.com>
2024-12-25 23:12:44 +0545

    Merging remote changes and adding local files

---
+++ index.java
@@ -0,0 +1,8 @@
+public class index {
+public static void main(String[] args) {
+String[] cars = {"Volvo", "BMW", "Ford"};
+for( int i = 0; i < cars.length; i++){
+System.out.println(cars[i]);
+}
+}
+}
