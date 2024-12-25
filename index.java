a8e21fdff38ae31cf4cab3f21ae619a4dc463c6b
Anjal <159101981+Anjalmanitimal@users.noreply.github.com>
2024-12-25 23:36:15 +0545

    Delete index.java

--- index.java
+++
@@ -1,8 +0,0 @@
-public class index {
-public static void main(String[] args) {
-String[] cars = {"Volvo", "BMW", "Ford"};
-for( int i = 0; i < cars.length; i++){
-System.out.println(cars[i]);
-}
-}
-}
